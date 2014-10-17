a6516607acd08329ee32cf2c1dd32afbd69676a9
helloiloveit <hello.i.love.it@gmail.com>
2014-10-17 08:27:50 +0700

    Revert 45beb3c..c91d30b
    This rolls back to commit 45beb3c66fb1805449ef70e5198e53ed9981f72a.

--- clear_database.py
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/python
-import optparse
-import os
-
-os.chdir('/Users/mac/Documents/shareknowledge')
-os.system('python web2py.py -S chuotnhat -M -R applications/chuotnhat/unittest/clear_db.py')
-
