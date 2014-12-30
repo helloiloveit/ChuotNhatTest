e4e4d9d1b35f39c6eafcf77e771d951429a9a41a
helloiloveit <hello.i.love.it@gmail.com>
2014-12-30 10:34:30 +0700

    update test

--- FunctionTest/function_test.py
+++ FunctionTest/function_test.py
@@ -4,14 +4,15 @@ import os
 
 def execute_test(option):
 	os.chdir('/Users/mac/Documents/shareknowledge/applications/chuotnhat/funtionalTest')
-	#os.system('python NotLoginUser.py local')
-	#os.system('python AfterLogin.py local')
-	os.system('python fb_page.py ' + option)
+	#os.system('python TestBasicFunction.py' + ' ' +  option)
+	os.system('python TestBasicFunctionFBCanvas.py' + ' ' +  option)
+	#os.system('python NotLoginUser.py'+' '+ option)
+	#os.system('python fb_page.py ' + option)
 
 def main():
 	parser = OptionParser()
 	parser.add_option("-t", "--target", dest="env",
-                  help="select target to run test against: local or test_version ",default = "local",  metavar="local")
+                  help="select target to run test against: local or deploy ",default = "local",  metavar="local")
 	(options, args) = parser.parse_args()
 	execute_test(options.env)
 
